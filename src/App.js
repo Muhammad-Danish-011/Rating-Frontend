e4f461cf247395c11a33228c88a63ca3910a99d3
Muhammad-Danish-011 <muhammad.danish@xloopdigital.com>
2023-06-17 04:54:44 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,74 @@
 import React, { useState } from 'react';
 import RatingUI from './components/rating';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#C2F8E4', // Replace with your desired background color
+    },
+  },
+});
 
 const App = () => {
   const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
 
   const handleRatingChange = (newValue) => {
     setRating(newValue);
   };
 
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleSubmitComment = () => {
+    if (comment.trim() !== '') {
+      setComments((prevComments) => [...prevComments, comment]);
+      setComment('');
+    }
+  };
+
   return (
-    <div>
-      <h1>Rating</h1>
-      <RatingUI value={rating} onChange={handleRatingChange} />
-      <p>Selected Rating: {rating}</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <Box
+          sx={{
+            
+            display: 'flex',
+            backgroundColor: '#C2F8E4',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: '16px',
+          }}
+        >
+          <h1>Rating</h1>
+          <RatingUI value={rating} onChange={handleRatingChange} />
+          <p>Selected Rating: {rating}</p>
+
+          <h2>Comments</h2>
+          <div>
+            {comments.map((comment, index) => (
+              <p key={index}>{comment}</p>
+            ))}
+          </div>
+
+          <textarea
+            value={comment}
+            onChange={handleCommentChange}
+            rows={4}
+            cols={50}
+            placeholder="Add a comment..."
+          />
+          <br />
+          <button onClick={handleSubmitComment}>Submit Comment</button>
+        </Box>
+      </div>
+    </ThemeProvider>
   );
 };
 
--- src/components/rating.jsx
+++ src/components/rating.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Rating from '@mui/material/Rating';
+import { styled } from '@mui/system';
+
+// Define custom styles using styled from @mui/system
+const StyledRating = styled(Rating)({
+  color: '#FFD700', // Set the color to a gold/yellow shade
+  fontSize: '2rem', // Increase the font size
+  // Add more custom styles here as needed
+});
 
 const RatingUI = ({ value, onChange }) => {
   return (
-    <Rating
+    
+    <StyledRating
+      
       name="rating"
       value={value}
       onChange={(event, newValue) => {
